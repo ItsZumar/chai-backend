2fc141fd02294d81e3a72e554411a4b88f4e7b3f
ItsZumar <zumarsaeed17@gmail.com>
2024-04-16 13:34:53 +0500

    created custom api response and error handling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+// By using Promises
+const asyncHandler = (requestHandler) => {
+  (req, res, err) => {
+    Promise.resolve(requestHandler(req, res, err)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// Using Try...Catch
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//       await fn(req, res, next);
+//     } catch (err) {
+//       res.status(err.code || 500).json({
+//         success: false,
+//         message: err.message,
+//       });
+//     }
+//   };
